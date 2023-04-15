e81a79bc653f2cae41c2fcf0d958cc8e3c454f8c
Kuldeep <kuldeep.mohanty2003@gmail.com>
2023-04-15 15:22:09 +0530

    Navbar Added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react'
+import{MenuIcon, XIcon} from '@heroicons/react/outline'
+import logo from '../assets/blood-type-b.png'
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+  return (
+    <div className='w-screen h-[80px] z-10 bg-white fixed drop-shadow-lg'>
+        <div className='px-2 flex justify-between items-center w-full h-full'>
+            <div className='flex items-center'>
+                {/* <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>BRAND.</h1> */}
+                <img className='h-12 w-12 mr-2' src={logo} alt="" />
+                <ul className='hidden md:flex font-bold text-1xl'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Donate</li>
+                    <li>Blood Urgency</li>
+                    <li>Contact</li>
+                </ul>
+                
+            </div>
+            <div className='hidden md:flex pr-4'>
+                    <button className='border-2 text-black px-8 py-3 mr-4'>Login</button>
+                    <button className='px-8 py-3'>Register</button>
+            </div>
+            <div className='md:hidden' onClick={handleClick}>
+                {!nav ? <MenuIcon className='w-5'/> : <XIcon className='w-5'/> } 
+            </div>
+        </div>
+
+        <ul className={!nav ? 'hidden' : 'absolute bg-white w-full px-8'}>
+                <li className='border-b-2 border-metal w-full'>Home</li>
+                <li className='border-b-2 border-metal w-full'>About</li>
+                <li className='border-b-2 border-metal w-full'>Donate</li>
+                <li className='border-b-2 border-metal w-full'>Blood Urgency</li>
+                <li className='border-b-2 border-metal w-full'>Contact</li>
+                <div className='flex flex-col my-4'>
+                    <button className='text-indigo-600 px-8 py-3 mb-4'>Login</button>
+                    <button className='px-8 py-3'>Register</button>
+                </div>
+        </ul>
+        
+
+    </div>
+    
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'blood': '#F45050',
+      'midnight': '#3C486B',
+      'metal': '#F0F0F0',
+      'ochre': '#F9D949',
+      'ivory': '#000000'
+    },
+  },
+  plugins: [],
+}
+
