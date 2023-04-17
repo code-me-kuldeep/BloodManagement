8d8ebb5aa20281a4543cdb1addc36eee4ab851a5
patita-paban <96936857+patita-paban@users.noreply.github.com>
2023-04-18 00:29:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import About from "./components/About"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar";
 import Login from './components/Login'
+import { Route,Routes,} from 'react-router-dom';
+
+
 function App() {
   return (
     <div className="App">
-      <>
+
+      
         <Navbar/>
-        <Hero/>
-        <About/>
-        <Login />
-      </>
+        <Hero />
+        <About />
+        
+        
+      
+         <Routes>
+      
+         <Route path='/Login' element={<Login />}></Route>
+          
+           
+         </Routes>  
+        
+         
+      
     </div>
   );
 }
