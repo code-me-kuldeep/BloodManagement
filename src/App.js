e7c3ae45287e9d73ee8b3e99f1e842371b2055aa
Revenant <dasdev.pratik@gmail.com>
2023-04-28 00:27:52 +0530

    updated to create browser api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import HOME from "./components/HOME/HOME";
-import { Route, Routes } from "react-router-dom";
-import Login from "./components/HOME/Login";
-import Register2 from "./components/HOME/Register2";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HOME />}></Route>
-        <Route path="/Login" element={<Login />}></Route>
-        <Route path="/Register2" element={<Register2 />}></Route>
-      </Routes>
+        <HOME />
     </>
   );
 }
